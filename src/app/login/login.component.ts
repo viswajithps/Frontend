f520407f8941de7539f08fb0bca2424372194dc5
Viswajith Puthenpurayil Sureshbabu(UST <284720@ust.com>
2024-08-28 09:55:33 +0530

    login added

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule,CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  model: any = {};
+
+  onSubmit() {
+    // Handle form submission
+    console.log('Login submitted', this.model);
+  }
+
+
+
+}
